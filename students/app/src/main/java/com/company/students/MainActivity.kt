70d70ef355a98f5558f3ea7cc1ad1973247c8f06
Maksim Krisanov <igrekzero@gmail.com>
2022-11-30 21:27:26 +0300

    try init firebase database

--- students/app/src/main/java/com/company/students/MainActivity.kt
+++ students/app/src/main/java/com/company/students/MainActivity.kt
@@ -2,10 +2,29 @@ package com.company.students
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = FirebaseDatabase.getInstance("https://kursach-a88dc-default-rtdb.europe-west1.firebasedatabase.app")
+
+
+        database.getReference("students").child("vanya").get().addOnSuccessListener {
+            if(it.exists()){
+                Log.i("govno", it.child("id").value.toString())
+            }
+        }.addOnFailureListener{
+            Log.i("govno", "13213")
+        }
     }
 }
